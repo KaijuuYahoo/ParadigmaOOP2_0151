e577ce4c5af5b4311d42ba54b0dd6039c0224690
KaijuuYahoo <muhammad.rasyid.ft24@mail.umy.ac.id>
2025-06-03 17:44:18 +0700

    Merapikan Program

--- Abstraksi.cpp
+++ Abstraksi.cpp
@@ -1,13 +1,11 @@
 #include<iostream>
 using namespace std;
-
 class AbstraksiKlas{
     private :
         string x,y;
     public :
         //Method Untuk mengisi nilai
         //Private member
-
         void setXY(string a, string b){
             x = a;
             y = b;
@@ -17,14 +15,12 @@ class AbstraksiKlas{
             cout << "x = " << x <<endl;
             cout << "y = " << y <<endl;            
         }
-   
 };
-
-
 int main(int argc, char const *argv[])
 {
     AbstraksiKlas ak;
-    ak.
+    ak.setXY("Yogyakarta","Kampus");
+    ak.display();
     return 0;
 }
 
