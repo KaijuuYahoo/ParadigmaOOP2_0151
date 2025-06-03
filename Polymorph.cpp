34ce3d1d7a845938995f2aab7e66771ef772c30f
KaijuuYahoo <muhammad.rasyid.ft24@mail.umy.ac.id>
2025-06-03 17:33:38 +0700

    Merapikan Program dengan menghapus beberapa spasi

--- Polymorph.cpp
+++ Polymorph.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 using namespace std;
-
 class seseorang{
     public:
         virtual void pesan() = 0;
@@ -8,21 +7,18 @@ class seseorang{
             //cout << "Pesan dari seseorang" <<endl;
         //}
 };
-
 class joko:public seseorang{
     public:
         void pesan(){
             cout << "Pesan dari Joko" <<endl;
         }
 };
-
 class lia:public seseorang{
     public:
         void pesan(){
             cout << "Pesan dari Lia" <<endl;
         }
 };
-
 
 int main()
 {
@@ -36,6 +32,7 @@ int main()
     objek ->pesan();
     a.seseorang::pesan();
     b.seseorang::pesan();
+
     return 0;
 }
 
